81439039ef2149dede8c3f7bf838b5c4470b37d8
EDGARLOZANO <edg283@gmail.com>
2018-03-28 13:19:07 -0700

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=app01',
+    'username' => 'app01',
+    'password' => 's3creta',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
