c08f516a29c7268bec33eadb48f4affa79aafc8a
EDGARLOZANO <edg283@gmail.com>
2018-04-05 12:54:27 -0600

    CRUD Usuarios

---
+++ models/Usuarios.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "usuarios".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property string $authKey
+ * @property string $accessToken
+ * @property int $role
+ */
+class Usuarios extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     *
+     *
+     */
+
+
+    public static function tableName()
+    {
+        return 'usuarios';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password'], 'required'],
+            [['role'], 'default', 'value' => null],
+            [['role'], 'integer'],
+            [['username', 'password', 'authKey'], 'string', 'max' => 255],
+            [['accessToken'], 'string', 'max' => 250],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'password' => 'Password',
+            'authKey' => 'Auth Key',
+            'accessToken' => 'Access Token',
+            'role' => 'Role',
+        ];
+    }
+
+
+}
