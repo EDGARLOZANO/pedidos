65dc7ea75dd0b1cd17d0bdbc6a3512612b428559
EDGARLOZANO <edg283@gmail.com>
2018-04-05 13:23:52 -0600

    CRUD Usuarios

---
+++ views/usuarios/_form.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Usuarios */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="usuarios-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?php echo $form->field($model, 'role')->dropDownList( ['1' => 'admin', '2' => 'user'] ); ?>
+
+
+    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
+// $form->field($model, 'authKey')->textInput(['maxlength' => true]) ?>
+//$form->field($model, 'accessToken')->textInput(['maxlength' => true]) ?>
